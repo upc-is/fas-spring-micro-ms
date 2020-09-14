739f64dc3b9bf591693cbae37dbaa4b8cd0bd927
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-09-14 18:53:43 -0500

    Adding Eureka server

---
+++ registry-service/src/test/java/pe/edu/upc/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
