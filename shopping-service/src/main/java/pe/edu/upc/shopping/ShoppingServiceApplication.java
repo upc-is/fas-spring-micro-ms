a2df0e01cb41b7d1d89fecdc10db580cc7eb793f
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-09-18 16:18:44 -0500

    Fixing Hystrix proxy

--- shopping-service/src/main/java/pe/edu/upc/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/pe/edu/upc/shopping/ShoppingServiceApplication.java
@@ -2,6 +2,7 @@ package pe.edu.upc.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
